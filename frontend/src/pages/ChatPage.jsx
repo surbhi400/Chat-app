76db0260730ff0bf5fc02db70c23773961ea853a
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-03-15 23:44:37 +0530

    adding more functionality

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,14 +3,20 @@ import { ChatState } from "../Context/ChatProvider";
 import SideDrawer  from '../components/Miscellaneous/SideDrawer';
 import MyChats  from '../components/Miscellaneous/MyChats';
 import ChatBox  from '../components/Miscellaneous/ChatBox';
+import { useState } from "react";
+
 const ChatPage = () => {
-   const { user } = ChatState()
+   const { user } = ChatState();
+   const [ fetchAgain, setFetchAgain ]= useState(false);
+
   return (
     <div style ={{width:'100%'}}>
         {user && <SideDrawer/>}
         <Box display="flex" justifyContent="space-between" w="100% "h="100vh" p="10px">
-          {user && <MyChats/>}
-          {user && <ChatBox/>}
+          {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
         </Box>
     </div>
   );
