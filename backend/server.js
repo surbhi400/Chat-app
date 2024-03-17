f57dc445f7bbbbff76e6a3a82d8ccb93695211f6
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-03-17 15:27:46 +0530

    typing animation implemented plus cleaning the socket

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,10 @@ io.on("connection", (socket)=>{
     socket.join(room);
     console.log("User Joined Room: " + room);
   });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (newMessageRecieved) => {
     var chat = newMessageRecieved.chat;
 
@@ -56,5 +60,8 @@ io.on("connection", (socket)=>{
       socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
-
+    socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
