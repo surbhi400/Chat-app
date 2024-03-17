117f18d27cb36769b596e1c487894efa995c22d7
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-03-17 14:38:31 +0530

    real time chat working

--- backend/server.js
+++ backend/server.js
@@ -35,4 +35,26 @@ const io= require('socket.io')(server, {
 });
 io.on("connection", (socket)=>{
     console.log('connected to socket.io');
+
+    socket.on('setup', (userData)=>{
+          socket.join(userData._id) // Join the room
+          console.log(userData._id);
+          socket.emit("connected");
+    });
+   socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
 });
